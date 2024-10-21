8f48a84fee9286f8d7ea735b594863ee7309a904
Mia <miaelanderson@gmail.com>
2024-10-21 09:42:34 -0500

    Initial Commit

---
+++ Roll the dice/Roll_the_diceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_diceApp.swift
+//  Roll the dice
+//
+//  Created by Mia Anderson on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_diceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
