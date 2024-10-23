e393e2a645d27e801355c387b62cb637f774cd3e
Mia <miaelanderson@gmail.com>
2024-10-23 09:38:33 -0500

    Added multiple random die effects

--- Roll the dice/ContentView.swift
+++ Roll the dice/ContentView.swift
@@ -12,25 +12,31 @@ struct ContentView: View {
     @State private var rotation = 0.0
     var body: some View {
         VStack {
-            Text("Dice Roll")
+            Text("Dice Roll!")
                 .font(.title).bold()
             Image("pips \(randomValue)")
                 .resizable()
                 .frame(width: 200, height: 200)
-                .rotationEffect(.degrees(rotation))
-                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
                 .padding()
                 .onTapGesture {
-                    randomValue = Int.random(in: 1...6)
-                    withAnimation {
+                    chooseRandom(times: 4)
+                    withAnimation(.interpolatingSpring(Spring(stiffness: 10, damping: 2))) {
                         rotation += 360
                     }
                 }
-            Spacer()
+                .rotationEffect(.degrees(rotation))
+                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
+        }
+    }
+    func chooseRandom(times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue = Int.random(in: 1...6)
+                chooseRandom(times: times - 1)
+            }
         }
     }
 }
-
 #Preview {
     ContentView()
 }
